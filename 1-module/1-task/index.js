d3cd3703140b149970690b6a83d80438a0917b5e
Paul Tveritinov <paul@nordigy.ru>
2021-05-18 11:26:49 +0300

    1st task of lesson 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,18 @@
 function factorial(n) {
-  // ваш код...
+
+  if( typeof n !== "number" )
+    return NaN;
+
+  if( n < 0 )
+    return null;
+
+  if( ( n === 0 ) || ( n === 1 ) )
+    return 1;
+
+  let retVal = n;
+
+  for( let i = ( n - 1); i > 0; --i )
+    retVal *= i;
+
+  return retVal;
 }
