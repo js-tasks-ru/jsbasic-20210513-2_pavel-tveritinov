51af0c400502e61a40d9ee43617b233fc370e26e
Paul Tveritinov <paul@nordigy.ru>
2021-05-25 09:58:49 +0300

    5th task of lesson 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,25 @@
 function getMinMax(str) {
-  // ваш код...
+
+//  if( typeof str !== "string" )
+//    return null;
+
+  let items = str.split( new RegExp( " |," ) );
+
+  let result = {};
+  
+  items.forEach( ( item ) =>{
+    if( !isFinite( item ) )
+      return;
+    
+    let numItem = Number( item );
+    
+    if( ( result.min === undefined ) || ( numItem < result.min ) )
+      result.min = numItem;
+        
+    if( ( result.max === undefined ) || ( numItem > result.max ) )
+      result.max = numItem;
+        
+  } );
+
+  return result;
 }
