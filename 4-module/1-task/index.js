03e9f8833bd74188ffd2f2622e615de990f264d4
Paul Tveritinov <paul@nordigy.ru>
2021-05-28 10:06:09 +0300

    1st task of lesson 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  if( !Array.isArray( friends ) ) {
+    return null;
+  }
+
+  let ulObj = document.createElement( 'ul' );
+
+  friends.forEach( ( item ) => {
+    let liObj = document.createElement( 'li' );
+    liObj.textContent = item.firstName + ' ' + item.lastName;
+    ulObj.appendChild( liObj );
+  } );
+
+  return ulObj;
 }
