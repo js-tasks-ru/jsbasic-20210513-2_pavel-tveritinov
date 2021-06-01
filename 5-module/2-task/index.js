0ba7b2c670db0fccbc87aeb1cc1a24ac883fea1a
Paul Tveritinov <paul@nordigy.ru>
2021-06-01 09:54:23 +0300

    2nd task of lesson 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let btnHide = document.querySelector( '.toggle-text-button' );
+
+  if( btnHide ) {
+    btnHide.addEventListener( 'click', ()=>{
+      let text = document.getElementById( 'text' );
+
+      if( text ) {
+        text.hidden = !text.hidden;
+      }
+    });
+  }
 }
