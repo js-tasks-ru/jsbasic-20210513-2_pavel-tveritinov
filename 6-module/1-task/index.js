86e76f7c9dfbfd52dc28ebad716f01392e5a1dba
Paul Tveritinov <paul@nordigy.ru>
2021-06-08 11:15:10 +0300

    1st task of lesson 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,53 @@
  *
  */
 export default class UserTable {
+  _deleteCol = "delete";
+
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this._columns = this._fillHeader( this.elem.createTHead(), rows );
+    this._fillRows( this.elem.createTBody(), rows );
+  }
+
+  _fillHeader( headerElem, rows ) {
+    let names = new Array();
+
+    for( let name in rows[ 0 ] ) {
+      names.push( name );
+    }
+
+    names.push( this._deleteCol ); // Close button
+    let header = '<tr>' + names.map( value => `<th>${value}</th>` ).join( '' ) + '</tr>';
+    headerElem.innerHTML = header;
+    return names;
+  }
+
+  _doDelete( event ) {
+    if( event.target.closest( 'button' ) ){
+      let rowClicked = event.target.closest( 'tr' );
+      if( rowClicked ) {
+        this.elem.deleteRow( rowClicked.rowIndex );
+      }
+    }
+  }
+
+  _fillRows(bodyElem, rows) {
+
+    for( let user of rows ) {
+      var userRow = bodyElem.insertRow();
+
+      for( let item of this._columns ) {
+        let nextCel = userRow.insertCell();
+        if( item === this._deleteCol ) {
+          var delButton = document.createElement('button');
+          delButton.textContent=`[X]`;
+          nextCel.appendChild(delButton);
+        } else {
+          nextCel.textContent = user[ item ];
+        }
+      }
+    }
+
+    bodyElem.addEventListener( 'click', (event) => { this._doDelete(event); } );
   }
 }
