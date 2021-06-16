b979ea5b4e37653579284d94e9a84e29370ef60c
Paul Tveritinov <paul@nordigy.ru>
2021-06-16 07:07:04 +0300

    2nd task of lesson 7

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,44 +2,14 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.fnKeyDown = (event) => {
+      if( event.code === 'Escape' ) {
+        this.close();
+      }
+    };
   }
 
-  setBody(node) {
-    this.myBody = node;
-  }
-
-  setTitle( title ) {
-    this.title = title;
-  }
-
-  open() {
-
-    this.elem = document.createElement('div');
-    this.elem.className = "modal";
-
-    this.elem.innerHtml = `<div class="modal__overlay"></div>
-  
-      <div class="modal__inner">
-        <div class="modal__header">
-          <button type="button" class="modal__close">
-            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
-          </button>
-  
-          <h3 class="modal__title"> ${this.title} </h3>
-        </div>
-        <div class="modal__body">
-        </div>
-      </div>`;
-
-    this._container = document.querySelector( ".container" );
-    if( !this._container ) {
-      throw "No container";
-    }
-
-    this._container.append( this.elem );
-
-    document.body.classList.add( "is-modal-open" );
-
+  _addBody() {
     if( this.myBody ) {
       let bodyPlace = this.elem.querySelector( ".modal__body" );
 
@@ -47,17 +17,73 @@ export default class Modal {
         bodyPlace.appendChild( this.myBody );
       }
     }
+  }
+
+  setBody(node) {
+    let modalOpened = document.body.classList.contains( "is-modal-open" );
+
+    if( this.myBody ) {
+      this.myBody.remove();
+      this.myBody = null;
+    }
+
+    this.myBody = node;
+
+    if( modalOpened ){
+      this._addBody();
+    }
+  }
+
+  setTitle( title ) {
+    this.title = title;
+
+    if( document.body.classList.contains( "is-modal-open" ) ){
+      let item = this.elem.querySelector( ".modal__title" );
+      if( item ) {
+        item.textContent = title;
+      }
+    }
+  }
+
+  open() {
+
+    this.elem = document.createElement('div');
+    this.elem.className = "modal";
+
+    this.elem.innerHTML = `<div class="modal__overlay"></div>
+  
+      <div class="modal__inner">
+        <div class="modal__header">
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+  
+          <h3 class="modal__title">${this.title}</h3>
+        </div>
+        <div class="modal__body">
+        </div>
+      </div>`;
+
+    document.body.classList.add( "is-modal-open" );
+    document.body.append( this.elem );
+    this._addBody();
 
     let closeBtn = this.elem.querySelector( ".modal__close" );
 
     if( closeBtn ){
       closeBtn.addEventListener( 'click', () => { this.close(); });
     }
+
+    document.addEventListener( 'keydown', this.fnKeyDown );
   }
 
   close() {
+    document.removeEventListener( 'keydown', this.fnKeyDown );
     document.body.classList.remove( "is-modal-open" );
-    this._container.removeChild( this.elem );
-    this.elem = null;
+
+    if( this.elem ) {
+      this.elem.remove();
+      this.elem = null;
+    }
   }
 }
